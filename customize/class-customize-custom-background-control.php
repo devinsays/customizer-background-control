37f642705d53b512d5b3dd9ba5183d1593d2350e
Devin Price <devin@wptheming.com>
2016-01-19 15:14:10 -0600

    Add settings to control.

--- customize/class-customize-custom-background-control.php
+++ customize/class-customize-custom-background-control.php
@@ -33,13 +33,13 @@ class Customize_Custom_Background_Control extends WP_Customize_Upload_Control {
 		parent::__construct( $manager, $id, $args );
 
 		$this->button_labels = array(
-			'select'       => __( 'Select Image', 'customizer-background' ),
-			'change'       => __( 'Change Image', 'customizer-background' ),
-			'remove'       => __( 'Remove', 'customizer-background' ),
-			'default'      => __( 'Default', 'customizer-background' ),
-			'placeholder'  => __( 'No image selected', 'customizer-background' ),
-			'frame_title'  => __( 'Select Image', 'customizer-background' ),
-			'frame_button' => __( 'Choose Image', 'customizer-background' ),
+			'select'       => __( 'Select Image', 'customizer-custom-background' ),
+			'change'       => __( 'Change Image', 'customizer-custom-background' ),
+			'remove'       => __( 'Remove', 'customizer-custom-background' ),
+			'default'      => __( 'Default', 'customizer-custom-background' ),
+			'placeholder'  => __( 'No image selected', 'customizer-custom-background' ),
+			'frame_title'  => __( 'Select Image', 'customizer-custom-background' ),
+			'frame_button' => __( 'Choose Image', 'customizer-custom-background' ),
 		);
 	}
 
@@ -51,8 +51,109 @@ class Customize_Custom_Background_Control extends WP_Customize_Upload_Control {
 	 * @return void
 	 */
 	public function enqueue() {
+
+		parent::enqueue();
+
 		wp_enqueue_script( 'customizer-custom-background-controls' );
 		wp_enqueue_style( 'customizer-custom-background-controls' );
+
+	}
+
+		/**
+	 * Add custom parameters to pass to the JS via JSON.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function to_json() {
+		parent::to_json();
+
+		$choices = $this->get_background_choices();
+
+		// Loop through each of the settings and set up the data for it.
+		foreach ( $this->settings as $setting_key => $setting_id ) {
+
+			$this->json[ $setting_key ] = array(
+				'link'  => $this->get_link( $setting_key ),
+				'value' => $this->value( $setting_key )
+			);
+
+			if ( 'repeat' === $setting_key ) {
+				$this->json[ $setting_key ]['choices'] = $choices['repeat'];
+			}
+			elseif ( 'size' === $setting_key ) {
+				$this->json[ $setting_key ]['choices'] = $choices['size'];
+			}
+			elseif ( 'attach' === $setting_key ) {
+				$this->json[ $setting_key ]['choices'] = $choices['attach'];
+			}
+			elseif ( 'position' === $setting_key ) {
+				$this->json[ $setting_key ]['choices'] = $choices['position'];
+			}
+
+		}
+	}
+
+	/**
+	 * Render a JS template for the content of the media control.
+	 *
+	 * @since 1.0.0
+	 */
+	public function content_template() {
+		?>
+		<# console.log( data ); #>
+
+		<label for="{{ data.settings['default'] }}-button">
+			<# if ( data.label ) { #>
+				<span class="customize-control-title">{{ data.label }}</span>
+			<# } #>
+			<# if ( data.description ) { #>
+				<span class="description customize-control-description">{{{ data.description }}}</span>
+			<# } #>
+		</label>
+
+
+		<# if ( data.size && data.size.choices ) { #>
+			<li class="custom-background-size">
+				<# if ( data.size.label ) { #>
+					<span class="customize-control-title">{{ data.size.label }}</span>
+				<# } #>
+				<select {{{ data.size.link }}}>
+					<# _.each( data.size.choices, function( label, choice ) { #>
+						<option value="{{ choice }}" <# if ( choice === data.size.value ) { #> selected="selected" <# } #>>{{ label }}</option>
+					<# } ) #>
+				</select>
+			</li>
+		<# } #>
+
+		<# if ( data.attach && data.attach.choices ) { #>
+			<li class="custom-background-attach">
+				<# if ( data.attach.label ) { #>
+					<span class="customize-control-title">{{ data.attach.label }}</span>
+				<# } #>
+				<select {{{ data.attach.link }}}>
+					<# _.each( data.attach.choices, function( label, choice ) { #>
+						<option value="{{ choice }}" <# if ( choice === data.attach.value ) { #> selected="selected" <# } #>>{{ label }}</option>
+					<# } ) #>
+				</select>
+			</li>
+		<# } #>
+
+		<# if ( data.position && data.position.choices ) { #>
+			<li class="custom-background-position">
+				<# if ( data.position.label ) { #>
+					<span class="customize-control-title">{{ data.position.label }}</span>
+				<# } #>
+				<select {{{ data.position.link }}}>
+					<# _.each( data.position.choices, function( label, choice ) { #>
+						<option value="{{ choice }}" <# if ( choice === data.position.value ) { #> selected="selected" <# } #>>{{ label }}</option>
+					<# } ) #>
+				</select>
+			</li>
+		<# } #>
+
+		<?php
 	}
 
 	/**
@@ -61,36 +162,36 @@ class Customize_Custom_Background_Control extends WP_Customize_Upload_Control {
 	 * @since 1.0.0
 	 * @return array
 	 */
-	public static function background_choices() {
+	public static function get_background_choices() {
 
 		$choices = array(
 			'repeat' => array(
-				'no-repeat' => __( 'No Repeat', 'customizer-background' ),
-				'repeat'    => __( 'Repeat All', 'customizer-background' ),
-				'repeat-x'  => __( 'Repeat X', 'customizer-background' ),
-				'repeat-y'  => __( 'Repeat Y', 'customizer-background' ),
-				'inherit'   => __( 'Inherit', 'customizer-background' ),
+				'no-repeat' => __( 'No Repeat', 'customizer-custom-background' ),
+				'repeat'    => __( 'Repeat All', 'customizer-custom-background' ),
+				'repeat-x'  => __( 'Repeat X', 'customizer-custom-background' ),
+				'repeat-y'  => __( 'Repeat Y', 'customizer-custom-background' ),
+				'inherit'   => __( 'Inherit', 'customizer-custom-background' ),
 			),
 			'size' => array(
-				'inherit' => __( 'Inherit', 'customizer-background' ),
-				'cover'   => __( 'Cover', 'customizer-background' ),
-				'contain' => __( 'Contain', 'customizer-background' ),
+				'inherit' => __( 'Inherit', 'customizer-custom-background' ),
+				'cover'   => __( 'Cover', 'customizer-custom-background' ),
+				'contain' => __( 'Contain', 'customizer-custom-background' ),
 			),
 			'attach' => array(
-				'inherit' => __( 'Inherit', 'customizer-background' ),
-				'fixed'   => __( 'Fixed', 'customizer-background' ),
-				'scroll'  => __( 'Scroll', 'customizer-background' ),
+				'inherit' => __( 'Inherit', 'customizer-custom-background' ),
+				'fixed'   => __( 'Fixed', 'customizer-custom-background' ),
+				'scroll'  => __( 'Scroll', 'customizer-custom-background' ),
 			),
 			'position' => array(
-				'left-top'      => __( 'Left Top', 'customizer-background' ),
-				'left-center'   => __( 'Left Center', 'customizer-background' ),
-				'left-bottom'   => __( 'Left Bottom', 'customizer-background' ),
-				'right-top'     => __( 'Right Top', 'customizer-background' ),
-				'right-center'  => __( 'Right Center', 'customizer-background' ),
-				'right-bottom'  => __( 'Right Bottom', 'customizer-background' ),
-				'center-top'    => __( 'Center Top', 'customizer-background' ),
-				'center-center' => __( 'Center Center', 'customizer-background' ),
-				'center-bottom' => __( 'Center Bottom', 'customizer-background' ),
+				'left-top'      => __( 'Left Top', 'customizer-custom-background' ),
+				'left-center'   => __( 'Left Center', 'customizer-custom-background' ),
+				'left-bottom'   => __( 'Left Bottom', 'customizer-custom-background' ),
+				'right-top'     => __( 'Right Top', 'customizer-custom-background' ),
+				'right-center'  => __( 'Right Center', 'customizer-custom-background' ),
+				'right-bottom'  => __( 'Right Bottom', 'customizer-custom-background' ),
+				'center-top'    => __( 'Center Top', 'customizer-custom-background' ),
+				'center-center' => __( 'Center Center', 'customizer-custom-background' ),
+				'center-bottom' => __( 'Center Bottom', 'customizer-custom-background' ),
 			),
 		);
 
--- customizer-custom-background.php
+++ customizer-custom-background.php
@@ -55,7 +55,11 @@ function custom_background_customize_register( $wp_customize ) {
 		'title' => esc_html__( 'Custom Background Testing', 'custom-background' )
 	) );
 
-	/*
+	// Registers example_background settings
+	$wp_customize->add_setting( 'example_background_image', array(
+		'default' => '',
+		'sanitize_callback' => 'esc_url'
+	) );
 
 	$wp_customize->add_setting( 'example_background_repeat', array(
 			'default' => 'no-repeat',
@@ -72,47 +76,29 @@ function custom_background_customize_register( $wp_customize ) {
 			'sanitize_callback' => 'sanitize_text_field'
 	) );
 
-	$wp_customize->add_setting( 'example_background_attach', array(
+	$wp_customize->add_setting( 'example_background_position', array(
 			'default' => 'center-center',
-			'sanitize_callback' => 'example_background_position'
+			'sanitize_callback' => 'sanitize_text_field'
 	) );
 
-	// Add an example background control
-	$wp_customize->add_control(
-		new Customizer_Custom_Background(
-			$wp_customize,
-			'example_background',
-			array(
-				'label'       => esc_html__( 'Example Background', 'custom-background' ),
-				'description' => __( 'Example background.', 'custom-background' ),
-				'section'     => 'example_background',
-
-				// Tie a setting (defined via `$wp_customize->add_setting()`) to the control.
-				'settings'    => array(
-					'repeat' => 'example_background_repeat',
-					'size' => 'example_background_size',
-					'attach' => 'example_background_attach',
-					'position' => 'example_background_position'
-				),
-			)
-		)
-	);
-
-	*/
-
-	$wp_customize->add_setting( 'example_background', array(
-		'default' => '',
-		'sanitize_callback' => 'esc_url'
-	) );
-
-	// Add an example background control
+	// Registers example_background control
 	$wp_customize->add_control(
 		new Customize_Custom_Background_Control(
 			$wp_customize,
 			'example_background',
 			array(
-				'label'       => esc_html__( 'Example Background', 'custom-background' ),
-				'section'     => 'custom_background_section'
+				'label'		=> esc_html__( 'Example Background', 'custom-background' ),
+				'section'	=> 'custom_background_section',
+				'setting'	=> 'example_background',
+				// Tie a setting (defined via `$wp_customize->add_setting()`) to the control.
+				'settings'    => array(
+					'image' => 'example_background_image',
+					'repeat' => 'example_background_repeat',
+					'size' => 'example_background_size',
+					'attach' => 'example_background_attach',
+					'position' => 'example_background_position'
+				),
+
 			)
 		)
 	);
